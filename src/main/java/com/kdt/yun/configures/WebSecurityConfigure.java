106002af2aaa6f49b5ac4aea51bd82b1c347cc41
yunyun <locodingve@gmail.com>
2021-11-11 19:44:58 +0900

    feat: 미션1 - admin 계정, user 계정 추가하기 - configure 메소드 활용해서 미션 수행하기

--- src/main/java/com/kdt/yun/configures/WebSecurityConfigure.java
+++ src/main/java/com/kdt/yun/configures/WebSecurityConfigure.java
@@ -1,10 +1,15 @@
 package com.kdt.yun.configures;
 
+import org.apache.commons.io.file.NoopPathVisitor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Created by yunyun on 2021/11/11.
@@ -13,6 +18,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //super.configure(auth);
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("user123")
+                .roles("USER");
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("admin123")
+                .roles("ADMIN");
+    }
 
     @Override
     public void configure(WebSecurity web){
@@ -31,4 +49,11 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
                     .permitAll()
                     .and();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+
 }
