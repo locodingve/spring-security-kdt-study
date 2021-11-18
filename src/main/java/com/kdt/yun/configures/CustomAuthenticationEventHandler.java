4f61639e6b08cf58afb919be3421e20fb52a0a5b
yunyun <locodingve@gmail.com>
2021-11-18 12:58:43 +0900

    feat: 이벤트 리스너 처리를 다른 스레드로 처리

--- src/main/java/com/kdt/yun/configures/CustomAuthenticationEventHandler.java
+++ src/main/java/com/kdt/yun/configures/CustomAuthenticationEventHandler.java
@@ -3,6 +3,7 @@ package com.kdt.yun.configures;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.authentication.event.AbstractAuthenticationFailureEvent;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.core.Authentication;
@@ -17,6 +18,7 @@ public class CustomAuthenticationEventHandler {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
+    @Async
     @EventListener
     public void handlerAuthenticationSuccessEvent(AuthenticationSuccessEvent event){
         Authentication authentication = event.getAuthentication();
--- src/main/java/com/kdt/yun/configures/WebMvcConfigure.java
+++ src/main/java/com/kdt/yun/configures/WebMvcConfigure.java
@@ -1,6 +1,7 @@
 package com.kdt.yun.configures;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * Created by yunyun on 2021/11/11.
  */
 
+@EnableAsync
 @Configuration
 public class WebMvcConfigure implements WebMvcConfigurer {
 
