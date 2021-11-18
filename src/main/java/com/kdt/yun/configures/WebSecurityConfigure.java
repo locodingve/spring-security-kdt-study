bba37d626d03f3e5d8d863a156550f0e4e05c657
yunyun <locodingve@gmail.com>
2021-11-18 16:55:00 +0900

    feat: DelegatingSecurityContextAsyncTaskExecutor

--- src/main/java/com/kdt/yun/configures/WebSecurityConfigure.java
+++ src/main/java/com/kdt/yun/configures/WebSecurityConfigure.java
@@ -1,8 +1,11 @@
 package com.kdt.yun.configures;
 
 import org.apache.commons.io.file.NoopPathVisitor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDecisionVoter;
 import org.springframework.security.access.expression.SecurityExpressionHandler;
@@ -17,12 +20,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
 import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.expression.WebExpressionVoter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * Created by yunyun on 2021/11/11.
@@ -33,9 +38,9 @@ import java.util.List;
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
 
 
-    public WebSecurityConfigure(){
-        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
-    }
+//    public WebSecurityConfigure(){
+//        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+//    }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -51,6 +56,23 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
                 .withUser("admin02")
                 .password("{noop}admin123")
                 .roles("ADMIN");
+    }
+
+    @Bean
+    @Qualifier("myAsyncTaskExecutor")
+    public ThreadPoolTaskExecutor threadPoolExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(5);
+        executor.setThreadNamePrefix("my-executor-");
+        return executor;
+    }
+
+    @Bean
+    public DelegatingSecurityContextAsyncTaskExecutor taskExecutor (
+            @Qualifier("myAsyncTaskExecutor") AsyncTaskExecutor delegate
+    ) {
+        return new DelegatingSecurityContextAsyncTaskExecutor(delegate);
     }
 
 
